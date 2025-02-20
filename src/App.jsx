728267c068be9906732005eaf3a2eda0362d7a15
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-02-20 20:59:25 +0100

    fixing fetch

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,10 @@ import ContactPage from "./pages/ContactPage";
 
 import DefaultLayout from "./layouts/DefaultLayout";
 import ProductsListPage from "./pages/ProductsListPage";
-import {
-  GlobalContextProvider,
-  useGlobalContext,
-} from "./context/GlobalContext";
+import { GlobalContextProvider } from "./context/GlobalContext";
 import ProductsManage from "./pages/ProductsManage";
-import { createClient } from "@supabase/supabase-js";
 
 function App() {
-  const { products, setProducts } = useGlobalContext();
-  const supabaseUrl = "https://hrrfdiwpvexyzhdioigj.supabase.co";
-  const supabaseKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhycmZkaXdwdmV4eXpoZGlvaWdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwNDk5MDUsImV4cCI6MjA1NTYyNTkwNX0.OKZU46kx-IK2EH5sSgxrCciaEe4ZkUjLgUY4qcVOf44";
-  const supabase = createClient(supabaseUrl, supabaseKey);
-  async function getProducts() {
-    const { data } = await supabase.from("products").select("*");
-    console.log(data);
-    setProducts(data);
-  }
-
-  getProducts();
   return (
     <GlobalContextProvider>
       <BrowserRouter>
