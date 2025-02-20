3f04e75294b22c378f0e957f43fd592e4cf7f4a6
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-02-20 19:45:03 +0100

    fixing fetch

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,40 @@ import DefaultLayout from "./layouts/DefaultLayout";
 import ProductsListPage from "./pages/ProductsListPage";
 import { GlobalContextProvider } from "./context/GlobalContext";
 import ProductsManage from "./pages/ProductsManage";
+import { createClient } from "@supabase/supabase-js";
+import { useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState("");
+  // supabase config
+  const supabaseUrl = "https://hrrfdiwpvexyzhdioigj.supabase.co";
+  const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhycmZkaXdwdmV4eXpoZGlvaWdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwNDk5MDUsImV4cCI6MjA1NTYyNTkwNX0.OKZU46kx-IK2EH5sSgxrCciaEe4ZkUjLgUY4qcVOf44";
+  const supabase = createClient(supabaseUrl, supabaseKey);
+  //   functions
+  async function getProducts() {
+    // const { data } = await supabase.from("products").select();
+    // setProducts(data);
+    fetch("https://hrrfdiwpvexyzhdioigj.supabase.co/rest/v1/products", {
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhycmZkaXdwdmV4eXpoZGlvaWdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwNDk5MDUsImV4cCI6MjA1NTYyNTkwNX0.OKZU46kx-IK2EH5sSgxrCciaEe4ZkUjLgUY4qcVOf44",
+        Authorization:
+          "Bearer " +
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhycmZkaXdwdmV4eXpoZGlvaWdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwNDk5MDUsImV4cCI6MjA1NTYyNTkwNX0.OKZU46kx-IK2EH5sSgxrCciaEe4ZkUjLgUY4qcVOf44",
+      },
+      body: JSON.stringify({
+        select: "*",
+      }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setProducts(data);
+      });
+  }
+
   return (
     <GlobalContextProvider>
       <BrowserRouter>
